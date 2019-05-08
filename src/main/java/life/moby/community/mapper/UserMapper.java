6da9dce2219981ed02f7b66736cd8da6a4a0c120
bullshit <objectives.milan@gmail.com>
2019-05-08 22:31:39 +0800

    add Flyway

--- src/main/java/life/moby/community/mapper/UserMapper.java
+++ src/main/java/life/moby/community/mapper/UserMapper.java
@@ -3,15 +3,27 @@ package life.moby.community.mapper;
 import life.moby.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
 
     /**
      * 插入
+     *
      * @param user
      */
     @Insert("insert into user(name, account_id,token,gmt_create,gmt_modified) " +
             "values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    /**
+     * 根据 token 获取 user
+     *
+     * @param token
+     * @return
+     */
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
